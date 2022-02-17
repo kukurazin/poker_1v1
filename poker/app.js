21c522323bb10cc279da5001f085ae6c3b2b4659
Valeri Beskor <61165697+kukurazin@users.noreply.github.com>
2022-02-17 18:13:45 +0200

    Update app.js

--- poker/app.js
+++ poker/app.js
@@ -2,48 +2,48 @@
 //I'll add filter for unique digits later.
 
 
-   let deck = ['🂱', '🃑', '🂡', '🃁', '🂲', '🃂', '🂢', '🃒', '🂣', '🂳', '🃃', '🃓', '🂤', '🂴', '🃄', '🃔', '🂥', '🂵', '🃅', '🃕', '🂦', '🂶', '🃆', '🃖', '🂧', '🂷', '🃇', '🃗', '🂨', '🂸', '🃈', '🃘', '🂩', '🂹', '🃉', '🃙', '🂪', '🂺', '🃊', '🃚', '🂫', '🂻', '🃋', '🃛', '🂮', '🂾', '🃎', '🃞', '🂭', '🂽', '🃍', '🃝'];
-
-   let back = '🂠';
-
-   function getCard() {
-      return deck.splice(Math.ceil(Math.random() * deck.length - 1), 1)[0]
-   }
-
-   let flop1 = getCard();
-   let flop2 = back;
-   let flop3 = back;
-   let turn = back;
-   let river = back;
-
-   let board = flop1 + flop2 + flop3 + turn + river;
-
-   card1 = getCard();
-   card2 = getCard();
-   card3 = getCard();
-   card4 = getCard();
-   card5 = getCard();
-   card6 = getCard();
-
-   let player1 = card1 + card2 + card3 + card4 + card5;
-   console.log('%c ' + player1 + ' ' + board, 'font-size:150px');;
-
-   let more = window.prompt("What's next?", "");
-
-
-   if (more === '6') {
-      player1 = player1 + card6;
-   } else {
-      [...more].forEach(el => player1 = player1.replace([...player1][el - 1], getCard()))
-   }
-
-   console.clear();
-
-   flop2 = getCard();
-   flop3 = getCard();
-   turn = getCard();
-   river = getCard();
-
-   board = flop1 + flop2 + flop3 + turn + river;
-
+   let deck = ['🂱', '🃑', '🂡', '🃁', '🂲', '🃂', '🂢', '🃒', '🂣', '🂳', '🃃', '🃓', '🂤', '🂴', '🃄', '🃔', '🂥', '🂵', '🃅', '🃕', '🂦', '🂶', '🃆', '🃖', '🂧', '🂷', '🃇', '🃗', '🂨', '🂸', '🃈', '🃘', '🂩', '🂹', '🃉', '🃙', '🂪', '🂺', '🃊', '🃚', '🂫', '🂻', '🃋', '🃛', '🂮', '🂾', '🃎', '🃞', '🂭', '🂽', '🃍', '🃝']; 
+ 
+   let back = '🂠'; 
+ 
+   function getCard() { 
+      return deck.splice(Math.ceil(Math.random() * deck.length - 1), 1)[0] 
+   } 
+ 
+   let flop1 = getCard(); 
+   let flop2 = back; 
+   let flop3 = back; 
+   let turn = back; 
+   let river = back; 
+ 
+   let board = flop1 + flop2 + flop3 + turn + river; 
+ 
+   card1 = getCard(); 
+   card2 = getCard(); 
+   card3 = getCard(); 
+   card4 = getCard(); 
+   card5 = getCard(); 
+   card6 = getCard(); 
+ 
+   let player1 = card1 + card2 + card3 + card4 + card5; 
+   console.log('%c ' + player1 + ' ' + board, 'font-size:150px');; 
+ 
+   let more = [...new Set(window.prompt("What's next?", ""))]; 
+ 
+ 
+   if (more[0] === '6') { 
+      player1 = player1 + card6; 
+   } else { 
+      [...more].forEach(el => player1 = player1.replace([...player1][el - 1], getCard())) 
+   } 
+ 
+   console.clear(); 
+ 
+   flop2 = getCard(); 
+   flop3 = getCard(); 
+   turn = getCard(); 
+   river = getCard(); 
+ 
+   board = flop1 + flop2 + flop3 + turn + river; 
+ 
    console.log('%c ' + player1 + ' ' + board, 'font-size:150px');
